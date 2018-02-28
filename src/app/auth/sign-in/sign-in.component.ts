d0985aac8fa32702980c580ca5dbb16e592f7936
Gururaj upparagummala <gururaj.upparagummala@DIT.LAN>
2018-02-28 20:05:27 +0530

    ADDED FORM VALIDATIONS PACKAGE

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-
+  formGroup: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ])
+    });
   }
 
+  onSubmit() {
+    console.log(this.formGroup);
+  }
 }
--- src/app/auth/sign-in/sign-in.module.ts
+++ src/app/auth/sign-in/sign-in.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 
 import { SignInRoutingModule } from './sign-in-routing.module';
 import { SignInComponent } from './sign-in.component';
+import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
 
 @NgModule({
   imports: [
     CommonModule,
-    SignInRoutingModule
+    SignInRoutingModule,
+    ReactiveFormsModule,
+    NgBootstrapFormValidationModule
   ],
   declarations: [SignInComponent]
 })
