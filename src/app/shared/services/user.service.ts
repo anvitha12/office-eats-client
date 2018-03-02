caa70ff00002ae1bb058c61069422fe0b7198db0
Gururaj upparagummala <gururaj.upparagummala@DIT.LAN>
2018-03-02 14:47:09 +0530

    removed formdata

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -20,27 +20,13 @@ export class UserService {
   }
 
   createUser(user: User) {
-    const form = new FormData();
-    form.append('user_role_type', user.user_role_type);
-    form.append('firstname', user.firstname);
-    form.append('email_id', user.email_id);
-    form.append('lastname', user.lastname);
-    form.append('gender', user.gender);
-    form.append('phone', user.phone);
-    form.append('password', user.password);
     return this.httpClient
       .post(
         this.createUserUrl,
-        form,
-        {
-          headers: new HttpHeaders({
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cache-Control': 'no-cache'
-          })
-        },
+        JSON.stringify(user)
       )
       .map(res => {
-        console.log(res);
+        return res;
       });
   }
 }
