299e4fcaecb45d91f8c372f674e085250fa9f943
gururaj <17214716+rajguru827@users.noreply.github.com>
2018-03-05 09:54:48 +0530

    Added validations and ViewContainerRef

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ToastModule } from 'ng2-toastr/ng2-toastr';
 import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ToastModule.forRoot(),
     NgBootstrapFormValidationModule.forRoot()
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { UserService } from '../../shared/services/user.service';
@@ -18,16 +18,23 @@ export class SignUpComponent implements OnInit {
     public toastr: ToastsManager,
     private titleService: Title,
     private userService: UserService,
-    private router: Router) { }
+    private router: Router,
+    public vcr: ViewContainerRef) {
+    this.toastr.setRootViewContainerRef(vcr);
+  }
 
   ngOnInit() {
     this.titleService.setTitle('Sign Up | OfficeEatz');
     this.formGroup = new FormGroup({
       first_name: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)
       ]),
       last_name: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)
       ]),
       gender: new FormControl('', [
         Validators.required
@@ -37,25 +44,27 @@ export class SignUpComponent implements OnInit {
         Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
       ]),
       phone: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(12)
       ]),
       password: new FormControl('', [
         Validators.required,
-        Validators.minLength(8),
-        Validators.maxLength(20)
+        Validators.minLength(6),
+        Validators.maxLength(30)
       ])
     });
   }
   onSubmit() {
     this.userService
-    .createUser(this.formGroup.value)
-    .subscribe(data=>{
-      if(data.status == 201){
-        this.toastr.success('Successfully registerd.', 'Success!');
-        this.router.navigate(['/sign-in']);
-      }else if(data.status == 400){
-        this.toastr.error('Email already exists.', 'Error!');
-      }
-    });
+      .createUser(this.formGroup.value)
+      .subscribe(data => {
+        if (data.status == 200) {
+          this.toastr.success('Successfully registerd.', 'Success!');
+          this.router.navigate(['/auth/sign-in']);
+        } else if (data.status == 400) {
+          this.toastr.error('Email already exists.', 'Error!');
+        }
+      });
   }
 }
