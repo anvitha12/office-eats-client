af4e49897638303c29590f159cb57a4fd3d3a9bf
gururaj <17214716+rajguru827@users.noreply.github.com>
2018-03-06 09:50:08 +0530

    Added create event component

--- src/app/manager/events/events-routing.module.ts
+++ src/app/manager/events/events-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventsComponent } from './events.component';
+import { CreateEventComponent } from './create-event/create-event.component';
 
 const routes: Routes = [
   {
-    path: '', component: EventsComponent
+    path: '', component: EventsComponent,
+  },
+  {
+    path: 'create', component: CreateEventComponent
   }
 ];
 
--- src/app/manager/events/events.module.ts
+++ src/app/manager/events/events.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { EventsRoutingModule } from './events-routing.module';
 import { EventsComponent } from './events.component';
+import { CreateEventComponent } from './create-event/create-event.component';
 
 @NgModule({
   imports: [
     CommonModule,
     EventsRoutingModule
   ],
-  declarations: [EventsComponent]
+  declarations: [EventsComponent, CreateEventComponent]
 })
 export class EventsModule { }
