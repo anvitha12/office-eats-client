00a02c24807d14ba6ada125e49b75ce795674cf2
Gururaj upparagummala <gururaj.upparagummala@DIT.LAN>
2018-03-16 19:02:07 +0530

    Module update

--- src/app/auth/forgot-password/forgot-password.module.ts
+++ src/app/auth/forgot-password/forgot-password.module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule , ReactiveFormsModule} from '@angular/forms';
-import { Title } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
 import { ForgotPasswordComponent } from './forgot-password.component';
-import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
 import { UserService } from '../../shared/services/user.service';
-
+import { SharedModule } from '../../shared/shared.module';
 @NgModule({
   imports: [
     CommonModule,
     ForgotPasswordRoutingModule,
-    ReactiveFormsModule,
-    HttpModule,
-    NgBootstrapFormValidationModule
+    SharedModule
   ],
   declarations: [ForgotPasswordComponent],
   providers: [
-    Title,
     UserService
   ]
 })
